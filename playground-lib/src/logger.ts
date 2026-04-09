21b2d1d7e2064a1dedade6a97a1829912630c9fb
Eduardo San Martin Morote <posva13@gmail.com>
2026-04-09 13:21:55 +0200

    refactor: fix go to definition

--- playground-lib/src/logger.ts
+++ playground-lib/src/logger.ts
@@ -1,14 +1,12 @@
-import type { Logger } from '@antfu/experimental-logs-sdk'
 import { createLogger } from '@antfu/experimental-logs-sdk'
 import { devReporter } from '@antfu/experimental-logs-sdk/dev-reporter'
 import { diagnostics } from './diagnostics'
 
-export const log: Logger<[typeof diagnostics]>
-  = createLogger({
-    diagnostics: [diagnostics],
-    // FIXME: I think we can put this directly within @antfu/experimental-logs-sdk, the lib author shouldn't have to import it separately
-    reporter: devReporter,
-  })
+export const log = createLogger({
+  diagnostics: [diagnostics],
+  // FIXME: I think we can put this directly within @antfu/experimental-logs-sdk, the lib author shouldn't have to import it separately
+  reporter: devReporter,
+})
 
 globalThis.nostics = log
 
