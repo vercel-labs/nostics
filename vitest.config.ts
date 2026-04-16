1f8dbb3608627757c804cd807803d818e0284c4e
Eduardo San Martin Morote <posva13@gmail.com>
2026-04-16 16:21:25 +0200

    test: more specific test filtering
    avoids running claude worktrees tests

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,12 +2,20 @@ import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
   test: {
+    include: [
+      'src/**/*.test.ts',
+      'test/**/*.test.ts',
+    ],
     server: {
       deps: {
         inline: ['vitest-package-exports'],
       },
     },
     typecheck: {
+      include: [
+        'src/**/*.test-d.ts',
+        'test/**/*.test-d.ts',
+      ],
       enabled: true,
     },
   },
